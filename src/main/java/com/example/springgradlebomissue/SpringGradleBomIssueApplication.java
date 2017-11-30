b9390730ac092342a86b6324625c6e2c6792f926
Kyle Anderson <andersonkyle@users.noreply.github.com>
2017-11-30 07:16:40 -0800

    Initial Commit

---
+++ src/main/java/com/example/springgradlebomissue/SpringGradleBomIssueApplication.java
@@ -0,0 +1,12 @@
+package com.example.springgradlebomissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradleBomIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGradleBomIssueApplication.class, args);
+	}
+}
